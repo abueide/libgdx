554fec7b493ab9290c9fafb94ba9bb286e3a3f85
Andrew Bueide <contact@abueide.com>
2020-02-17 01:43:23 -0600

    Naive collision implemented

--- gdx/src/com/badlogic/gdx/maps/tiled/renderers/OrthogonalTiledMapRenderer.java
+++ gdx/src/com/badlogic/gdx/maps/tiled/renderers/OrthogonalTiledMapRenderer.java
@@ -106,6 +106,7 @@ public class OrthogonalTiledMapRenderer extends BatchTiledMapRenderer {
 					final int rotations = cell.getRotation();
 
 					TextureRegion region = tile.getTextureRegion();
+					fixBleeding(region);
 
 					float x1 = x + tile.getOffsetX() * unitScale;
 					float y1 = y + tile.getOffsetY() * unitScale;
@@ -211,4 +212,15 @@ public class OrthogonalTiledMapRenderer extends BatchTiledMapRenderer {
 			y -= layerTileHeight;
 		}
 	}
+
+    private void fixBleeding(TextureRegion region) {
+        float fix = 0.01f;
+        float x = region.getRegionX();
+        float y = region.getRegionY();
+        float width = region.getRegionWidth();
+        float height = region.getRegionHeight();
+        float invTexWidth = 1f / region.getTexture().getWidth();
+        float invTexHeight = 1f / region.getTexture().getHeight();
+        region.setRegion((x + fix) * invTexWidth, (y + fix) * invTexHeight, (x + width - fix) * invTexWidth, (y + height - fix) * invTexHeight); // Trims Region
+    }
 }
